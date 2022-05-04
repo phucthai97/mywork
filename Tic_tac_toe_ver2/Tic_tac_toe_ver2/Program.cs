988f9e8dbbecf11ea92abc994756d91c9d62a07a
JonnyNguyen97 <100581351+JonnyNguyen97@users.noreply.github.com>
2022-05-04 09:31:05 +0700

    Tic Tac Toe Ver2.1

--- Tic_tac_toe_ver2/Tic_tac_toe_ver2/Program.cs
+++ Tic_tac_toe_ver2/Tic_tac_toe_ver2/Program.cs
@@ -6,27 +6,83 @@ namespace Tic_tac_toe_ver2
     {
         static void Main(string[] args)
         {
-            string[,] array_game =
-            {
-                {"1", "2", "3", "4", "5"},
-                {"6", "7", "8", "9", "10"},
-                {"11", "12", "13", "14", "15"},
-                {"16", "17", "18", "19", "20"},
-                {"21", "22", "23", "24", "25"}
-            };
+            //string[,] array_game =
+            //{
+            //    {"1", "2", "3", "4", "5"},
+            //    {"6", "7", "8", "9", "10"},
+            //    {"11", "12", "13", "14", "15"},
+            //    {"16", "17", "18", "19", "20"},
+            //    {"21", "22", "23", "24", "25"}
+            //};
 
             int player = 0;
-            string sign_player = " "; //X or O
-            bool choose_player_flat = false;
             int check_win = 0;
-            bool win = false;
+            int choose_size = 0;
+            int size_board = 1;
+            int score_to_win = 3;
+
+            string sign_player = " "; //X or O
             string player_name = "";
             string player_win = "";
-            //bool choose_number_flat = false;
-            //string input_player = " ";
+
+            bool choose_player_flat = false;
+            bool win = false;
+            bool choose_size_flat = false;
+
             Console.WriteLine("\n     #########################################");
             Console.WriteLine("     #########----- TIC TAC TOE -----#########");
             Console.WriteLine("     #########################################\n");
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Choose size board");
+                    Console.WriteLine("1.Size 5x5");
+                    Console.WriteLine("2.Size 7x7");
+                    Console.WriteLine("3.Size 9x9");
+                    Console.WriteLine("4.Size 11x11\n");
+                    choose_size = Int32.Parse(Console.ReadLine());
+                    if (choose_size == 1)
+                    {
+                        //string[,] array_game = new string[5,5];
+                        choose_size_flat = true;
+                        size_board = 5;
+                    }
+                    else if (choose_size == 2)
+                    {
+                        choose_size_flat = true;
+                        size_board = 7;
+                    }
+                    else if (choose_size == 3)
+                    {
+                        choose_size_flat = true;
+                        size_board = 9;
+                    }
+                    else if (choose_size == 4)
+                    {
+                        choose_size_flat = true;
+                        size_board = 11;
+                    }
+                    else
+                    {
+                        choose_size_flat = false;
+                        Console.WriteLine("Size is not correct. Please choose size above");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Input is not number. Please choose size above");
+                }
+            } while (!choose_size_flat);
+
+            string[,] array_game = new string[size_board, size_board];
+            array_game = Make_Size_Board(size_board);
+            //for (int i = 0; i < array_game.GetLength(0); i++)
+            //{
+            //    Console.WriteLine(array_game[6, i]);
+            //}
+
             do
             {
                 Console.WriteLine("Please chosse player to play first:");
@@ -60,7 +116,7 @@ namespace Tic_tac_toe_ver2
 
             do
             {
-                Print_Board(array_game, player_name, sign_player, win);
+                Print_Board(array_game, player_name, sign_player, win, size_board);
 
                 Doing_Game(array_game, sign_player);
 
@@ -197,7 +253,7 @@ namespace Tic_tac_toe_ver2
                     sign_player = "X";
                 }
 
-                check_win = Check_To_Win(array_game);
+                check_win = Check_To_Win(array_game, score_to_win);
 
                 if (check_win == 1)
                 {
@@ -214,17 +270,33 @@ namespace Tic_tac_toe_ver2
                     win = false;
                 }
             } while (!win);
-            Print_Board(array_game, player_name, sign_player, win);
+            Print_Board(array_game, player_name, sign_player, win, size_board);
             Console.WriteLine("\n###############################");
             Console.WriteLine($"####  The winner is {player_win}  ####");
             Console.WriteLine("###############################\n");
         }
 
+        #region Make Size Board Game
+        private static string[,] Make_Size_Board(int size_board)
+        {
+            int push_value_size = 1;
+            string[,] array_game = new string[size_board, size_board];
+            for (int i = 0; i < size_board; i++)
+            {
+                for (int j = 0; j < size_board; j++)
+                {
+                    array_game[i, j] = push_value_size.ToString();
+                    push_value_size++;
+                }
+            }
+            return array_game;
+        }
+        #endregion
+
         #region Check Win
-        private static int Check_To_Win(string[,] array_game)
+        private static int Check_To_Win(string[,] array_game, int score_to_win)
         {
             int check_win = 0;
-            int score_to_win = 3;
             string[] sign_player_arr = { "X", "O" };
             Console.WriteLine($"This for test { array_game.GetLength(0)} and  {array_game.GetLength(1)} and h is {sign_player_arr.GetLength(0)}");
             for (int h = 0; h < sign_player_arr.GetLength(0); h++)
@@ -342,14 +414,28 @@ namespace Tic_tac_toe_ver2
         #endregion
 
         #region Print Board on screen
-        static void Print_Board(string[,] Barray_game, string player_name, string sign_player, bool win)
+        static void Print_Board(string[,] Barray_game, string player_name, string sign_player, bool win, int size_board)
         {
-            Console.WriteLine("\n     |-------|-------|-------|-------|-------|");
+            //Draw top borders
+            Console.Write("\n     |");
+            for (int k = 0; k < size_board; k++)
+            {
+                Console.Write("-------|");
+            }
+            Console.WriteLine("");
+            //End Draw top borders
+
             for (int i = 0; i < Barray_game.GetLength(0); i++)
             {
-
-                Console.WriteLine("     |       |       |       |       |       |");
+                //Draw collum 
+                Console.Write("     |");
+                for (int k = 0; k < size_board; k++)
+                {
+                    Console.Write("       |");
+                }
+                Console.WriteLine("");
                 Console.Write("     |   ");
+                //End Draw collum
                 for (int j = 0; j < Barray_game.GetLength(1); j++)
                 {
                     if (Barray_game[i, j].Length == 1)
@@ -396,10 +482,42 @@ namespace Tic_tac_toe_ver2
                             Console.Write("  |   ");
                         }
                     }
+                    else if (Barray_game[i, j].Length == 3)
+                    {
+                        if (Barray_game[i, j] == "X")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write($"{Barray_game[i, j]}");
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            Console.Write(" |   ");
+                        }
+                        else if (Barray_game[i, j] == "O")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.Write($"{Barray_game[i, j]}");
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            Console.Write(" |   ");
+                        }
+                        else
+                        {
+                            Console.Write($"{Barray_game[i, j]}");
+                            Console.Write(" |   ");
+                        }
+                    }
                 }
                 Console.WriteLine("");
-                Console.WriteLine("     |       |       |       |       |       |");
-                Console.WriteLine("     |-------|-------|-------|-------|-------|");
+                Console.Write("     |");
+                for (int n = 0; n < size_board; n++)
+                {
+                    Console.Write("       |");
+                }
+                Console.WriteLine("");
+                Console.Write("     |");
+                for (int k = 0; k < size_board; k++)
+                {
+                    Console.Write("-------|");
+                }
+                Console.WriteLine("");
             }
             if (win == false)
                 Console.WriteLine($"\n-> Player {player_name} (with sign {sign_player}) turn, Please enter number:");
